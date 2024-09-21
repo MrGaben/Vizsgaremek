43b692806c15f97e9e44492601c031d27a9e353d
Imii <76595709+iambestaim@users.noreply.github.com>
2024-09-22 00:53:56 +0200

    Maybe nothing changed

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const checkAuth = require('./controllers/checkAuth.js');
 const authController = require('./controllers/authController.js');
+const router = express.Router();
 const userController = require('./controllers/userController.js');
+const exphbs = require('express-handlebars');
 
 dotenv.config({ path: './.env' });
 
@@ -16,7 +18,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
+
+
+// Middleware a form és JSON adatok kezeléséhez
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Regisztrálj a Handlebars segédet
+const hbs = exphbs.create({
+    helpers: {
+        eq: (a, b) => a === b
+    }
+});
+
+app.engine('hbs', hbs.engine);
+
+app.set('views', './views');
+
 app.set('view engine', 'hbs');
+
 
 // Middleware to check if user is authenticated
 app.use(checkAuth);
@@ -54,6 +74,11 @@ app.get("/loggedin", (req, res) => {
     if (!req.user) return res.redirect('/login');
     res.render('loggedin');
 });
+
+// Add this route for the admin page
+app.get("/admin", userController.getAllUsers);
+
+app.post('/admin/toggleRole', userController.toggleRole);
 
 app.post("/auth/register", authController.register);
 app.post("/auth/login", authController.login);
